aa2098ab143c77387c9f8df37fe2d1caee903fa0
Joshua Murphy <joshua.murphy@lasp.colorado.edu>
2013-08-02 11:06:18 -0600

    working on integrating enlil reader

--- CDAweb/CDAweb_Files.cmake
+++ CDAweb/CDAweb_Files.cmake
@@ -1,4 +1,4 @@
-#Splitting out files list
+#Files List for CDAweb Tool Kit
 
 if(NOT CDAWebTK_SOURCE_DIR)
     set(CDAWebTK_SOURCE_DIR ${CMAKE_CURRENT_LIST_DIR})
@@ -7,7 +7,7 @@ endif()
 
 # wrap QT
 QT4_WRAP_CPP(
-    MOC_BUILT_SOURCES
+    CDAWebTK_MOC_BUILT_SOURCES
         ${CDAWebTK_SOURCE_DIR}/src/filterNetworkAccessModule.h
         ${CDAWebTK_SOURCE_DIR}/src/ScInfoPropWidget.h
         ${CDAWebTK_SOURCE_DIR}/src/status.h
@@ -19,49 +19,64 @@ QT4_WRAP_CPP(
 
 # Wrap UI
 QT4_WRAP_UI(
-    UI_BUILT_SOURCES
-    ${CDAWebTK_SOURCE_DIR}/ui/ScInfoPropWidget.ui
-    ${CDAWebTK_SOURCE_DIR}/ui/status.ui
-    ${CDAWebTK_SOURCE_DIR}/ui/pathSelector.ui
+    CDAWebTK_UI_BUILT_SOURCES
+        ${CDAWebTK_SOURCE_DIR}/ui/ScInfoPropWidget.ui
+        ${CDAWebTK_SOURCE_DIR}/ui/status.ui
+        ${CDAWebTK_SOURCE_DIR}/ui/pathSelector.ui
     )
 
 # Property Widgets
-set(outifaces0)
-set(outsrcs0)
-ADD_PARAVIEW_PROPERTY_WIDGET(outifaces0 outsrcs0
+set(CDAWebTK_outifaces0)
+set(CDAWebTK_outsrcs0)
+ADD_PARAVIEW_PROPERTY_WIDGET(CDAWebTK_outifaces0 CDAWebTK_outsrcs0
         TYPE "SpaceCraftInfoProperties"
         CLASS_NAME "ScInfoPropWidget")
 
-set(outifaces1)
-set(outsrcs1)
-ADD_PARAVIEW_PROPERTY_WIDGET(outifaces1 outsrcs1
+set(CDAWebTK_outifaces1)
+set(CDAWebTK_outsrcs1)
+ADD_PARAVIEW_PROPERTY_WIDGET(CDAWebTK_outifaces1 CDAWebTK_outsrcs1
         TYPE "PathSelectionProperty"
         CLASS_NAME "pathSelector")
 
 # Set up files for compilation
-set(SM_XML
-                ${CDAWebTK_SOURCE_DIR}/xml/SpaceCraftInfoFilter.xml
-                ${CDAWebTK_SOURCE_DIR}/xml/SpaceCraftInfoSource.xml
+set(CDAWebTK_SM_XML
+        ${CDAWebTK_SOURCE_DIR}/xml/SpaceCraftInfoFilter.xml
+        ${CDAWebTK_SOURCE_DIR}/xml/SpaceCraftInfoSource.xml
     )
 
-set(PROP_ifaces ${outifaces0} ${outifaces1})
-set(PROP_src ${outsrcs0} ${outsrcs1})
-set(QT_Auto_src ${MOC_BUILT_SOURCES} ${UI_BUILT_SOURCES})
-set(PLUGIN_src
-                ${CDAWebTK_SOURCE_DIR}/src/filterNetworkAccessModule.cxx
-                ${CDAWebTK_SOURCE_DIR}/src/vtkSpaceCraftInfo.cxx
-                ${CDAWebTK_SOURCE_DIR}/src/ScInfoPropWidget.cxx
-                ${CDAWebTK_SOURCE_DIR}/src/status.cxx
-                ${CDAWebTK_SOURCE_DIR}/src/filedownloader.cxx
-                ${CDAWebTK_SOURCE_DIR}/src/timefithandler.cxx
-                ${CDAWebTK_SOURCE_DIR}/src/BadDataHandler.cxx
-                ${CDAWebTK_SOURCE_DIR}/src/cdfDataReader.cxx
-                ${CDAWebTK_SOURCE_DIR}/src/omitBDHandler.cxx
-                ${CDAWebTK_SOURCE_DIR}/src/pathSelector.cxx
-                ${CDAWebTK_SOURCE_DIR}/src/newFileChooserWidget.cxx
+set(CDAWebTK_PROP_IFACES
+        ${CDAWebTK_outifaces0}
+        ${CDAWebTK_outifaces1}
     )
 
-set(PLUGIN_SM_src
-                    ${CDAWebTK_SOURCE_DIR}/src/vtkSpaceCraftInfoFilter.cxx
-                    ${CDAWebTK_SOURCE_DIR}/src/vtkSpaceCraftInfoSource.cxx
+set(CDAWebTK_PROP_SRC
+        ${CDAWebTK_outsrcs0}
+        ${CDAWebTK_outsrcs1}
+    )
+
+set(CDAWebTK_QT_SRC
+        ${CDAWebTK_MOC_BUILT_SOURCES}
+        ${CDAWebTK_UI_BUILT_SOURCES}
+    )
+
+set(CDAWebTK_SRC
+        ${CDAWebTK_SOURCE_DIR}/src/filterNetworkAccessModule.cxx
+        ${CDAWebTK_SOURCE_DIR}/src/vtkSpaceCraftInfo.cxx
+        ${CDAWebTK_SOURCE_DIR}/src/ScInfoPropWidget.cxx
+        ${CDAWebTK_SOURCE_DIR}/src/status.cxx
+        ${CDAWebTK_SOURCE_DIR}/src/filedownloader.cxx
+        ${CDAWebTK_SOURCE_DIR}/src/timefithandler.cxx
+        ${CDAWebTK_SOURCE_DIR}/src/BadDataHandler.cxx
+        ${CDAWebTK_SOURCE_DIR}/src/cdfDataReader.cxx
+        ${CDAWebTK_SOURCE_DIR}/src/omitBDHandler.cxx
+        ${CDAWebTK_SOURCE_DIR}/src/pathSelector.cxx
+        ${CDAWebTK_SOURCE_DIR}/src/newFileChooserWidget.cxx
+        ${CDAWebTK_MOC_BUILT_SOURCES}
+        ${CDAWebTK_UI_BUILT_SOURCES}
+        ${CDAWebTK_QT_SRC}
+    )
+
+set(CDAWebTK_SM_SRC
+        ${CDAWebTK_SOURCE_DIR}/src/vtkSpaceCraftInfoFilter.cxx
+        ${CDAWebTK_SOURCE_DIR}/src/vtkSpaceCraftInfoSource.cxx
     )
