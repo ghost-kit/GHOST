aa076ae3c6e85094e9c8971834557d30bd8b4d24
Joshua Murphy <joshua.murphy@lasp.colorado.edu>
2013-08-07 20:22:27 -0600

    adding jpeg fix

--- CMake/jpeg.cmake
+++ CMake/jpeg.cmake
@@ -1,3 +1,4 @@
+
 add_external_project_or_use_system(
     jpeg
 
@@ -5,11 +6,7 @@ add_external_project_or_use_system(
           ${CMAKE_COMMAND} -E make_directory "${install_location}/bin" &&
           ${CMAKE_COMMAND} -E make_directory "${install_location}/man/man1"
 
-  CONFIGURE_COMMAND <SOURCE_DIR>/configure
-                    --prefix=<INSTALL_DIR>
-                    --disable-shared
-                    --mandir=<INSTALL_DIR>/man
-                    --bindir=<INSTALL_DIR/bin
 
-  INSTALL_COMMAND make install
+    INSTALL_COMMAND echo "Installed"
+
 )
