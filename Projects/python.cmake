189828543036270578f82b567f2fb633aff2d9db
Joshua Murphy <Joshua.murphy@lasp.colorado.edu>
2016-01-27 10:01:13 -0700

    working on fix for pytyhon

--- Projects/python.cmake
+++ Projects/python.cmake
@@ -12,6 +12,7 @@ add_external_project_or_use_system(python
   CONFIGURE_COMMAND <SOURCE_DIR>/configure
                     --prefix=<INSTALL_DIR>
                     --enable-unicode
-                    ${libtype}
+                    --enable-shared
+		    #${libtype}
   )
 set (pv_python_executable "${install_location}/bin/python" CACHE INTERNAL "" FORCE)
