f377011b8cc5ffb01230231121a5bef09b94c40a
Joshua Murphy <Joshua.murphy@lasp.colorado.edu>
2016-02-05 14:20:33 -0700

    updating build system

---
+++ Projects/GKbuildDep.cmake
@@ -0,0 +1,14 @@
+add_external_project(
+    GKbuildDep
+    DEPENDS cdf io DateTime boost qt paraview cxform
+
+   CONFIGURE_COMMAND
+   echo "No Configure Command"
+
+   BUILD_COMMAND
+   echo "No Build"
+
+   INSTALL_COMMAND
+   echo "No Install Command"
+
+    )
